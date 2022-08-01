c07b878ad92509a8d9c0d284da2340a6530c6afc
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-08-01 23:51:01 +0900

    An implementation of Interface Segregation Principle (ISP)

---
+++ generators_iterators_asynchronous_programming/generator_test.py
@@ -0,0 +1,12 @@
+test_list = [1, 3, 5, 7, 9, 11, 13, 17, 18, 20, 22]
+
+
+even_number = next(
+    (
+        i
+        for i in test_list
+        if i % 2 == 0
+    ), None
+)
+
+print(even_number)

8c58c064832d4126d3fcd5cdd0d12b9b17d5fe7c
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-08-15 22:48:00 +0900

    decorator function 1

---
+++ decorators/decorator_function_1.py
@@ -0,0 +1,32 @@
+# decorator_function_1.py
+import logging
+from functools import wraps
+
+
+class ControlledException(Exception):
+    """ A generic exception on the program's domain."""
+
+
+def retry(operation):
+    @wraps(operation)
+    def wrapped(*args, **kwargs):
+        last_raised = None
+        RETRIES_LIMIT = 3
+        for _ in range(RETRIES_LIMIT):
+            try:
+                return operation(*args, **kwargs)
+            except ControlledException as e:
+                logging.info("retrying %s", operation.__qualname__)
+                last_raised = e
+
+        raise last_raised
+
+    return wrapped
+
+
+@retry
+def run_operation(task):
+    """Run a particular task, simulating some failures on its exception."""
+    return task.run()
+
+# run_operation = retry(run_operation)

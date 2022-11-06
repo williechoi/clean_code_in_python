cf4a5e758b593d6e4b9f7784a3c4227c3dacc155
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-11-06 23:32:26 +0900

    add one more example to coordinate 2D

--- descriptors/slot_descriptor.py
+++ descriptors/slot_descriptor.py
@@ -11,7 +11,13 @@ class Coordinate2D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.lat}, {self.long})"
 
+    def __str__(self):
+        return f'({self.lat}, {self.long})'
+
 
 coord = Coordinate2D(2.2, 5.5)
 print(coord)
+
+coord = Coordinate2D(4.4, -0.8)
+print('%r' % coord)
 
